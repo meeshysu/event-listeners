79dcdc80941606e9fdb1e92d3758aa179f06499d
Michelle Beshears <meeshysu@gmail.com>
2018-09-18 20:01:25 -0500

    event listeners working for delete button

--- main.js
+++ main.js
@@ -3,6 +3,17 @@ const notesInputElem = document.getElementById("notesInput");
 
 const submitToDoButtonElem = document.getElementById("submitToDoButton");
 
+const activateDeletes = () => {
+    const deleteButtons = document.getElementsByClassName('deleteButton');
+    console.log(deleteButtons);
+    
+    for (let i = 0; i < deleteButtons.length; i++) {
+        const element = deleteButtons[i];
+        element.addEventListener("click", () => {
+            console.log("they clicked delete!!!");
+        })
+    }
+}
 
 
 printToDom = (stringToPrint, whereToPrint) => {
@@ -14,10 +25,11 @@ const buildNewToDoCard = (toDo, notes) => {
     <div class="card-body">
     <h5 class="card title">${toDo}</h5>
       <p class="card-text">${notes}</p>
-      <a href="#" class="btn btn-primary">Go Somewhere</a>
+      <a href="#" class="btn btn-primary deleteButton">Delete This Ugly Shit MEOW</a>
      </div>
   </div>`;
     printToDom(domString, "toDooCards");
+    activateDeletes();
 }
 
 submitToDoButtonElem.addEventListener("click", (e) => {
